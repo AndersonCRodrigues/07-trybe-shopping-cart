9511099b99a30e6ec9a5e723b26825d83dc756f8
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-10-11 15:02:04 -0300

    adicina testes de fetchProducts segundo requisito 01

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -1,8 +1,28 @@
+const { expect } = require('@jest/globals');
 require('../mocks/fetchSimulator');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  // fail('Teste vazio');
+  test('verifica se fetchProducts é uma função', () => {
+    expect.assertions(1);
+    expect(typeof fetchProducts).toBe('function');
+  });
+  test('verifica se fetch é chamada após executar fetchProducts("computador");', () => {
+    expect.assertions(2);
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
+  test('verifica se fetchProducts() retorna erro "You must provide an url"', () => {
+    expect.assertions(1);
+    try{
+      fetchProducts();
+    } catch(error) {
+      expect(error).toEqual(new Error('You must provide an url'));
+    }
+  });
 });

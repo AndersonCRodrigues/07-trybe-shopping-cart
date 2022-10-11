3d28886f86afa26c13b9b69bc0408e25a6834cb0
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-10-11 20:08:04 -0300

    adiciona teste saveCartItems

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -1,3 +1,4 @@
+const { expect } = require('@jest/globals');
 const localStorageSimulator = require('../mocks/localStorageSimulator');
 const saveCartItems = require('../helpers/saveCartItems');
 
@@ -5,5 +6,17 @@ localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  // fail('Teste vazio');
+  test('Teste se, ao executar saveCartItems com um cartItem como argumento, o método localStorage.setItem é chamado', () => {
+    expect.assertions(1);
+    saveCartItems(cartItem);
+    expect(localStorage).toHaveBeenCalled();
+  });
+  test('Teste se, ao executar saveCartItems com um cartItem como argumento, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro a chave "cartItems" e o segundo sendo o valor passado como argumento para saveCartItems', () => {
+    expect.assertions(1);
+    saveCartItems(cartItem);
+    const key = 'cartItems';
+    const value = 'saveCartItems';
+    expect(localStorage.setItem).toHaveBeenLastCalledWith(key, value);
+  });
 });

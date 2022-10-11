bc15970a433349f098ef92d2072aa820e6955545
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-10-11 15:34:44 -0300

    adiciona função fetchItem

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,10 @@
-const fetchItem = () => {
+const fetchItem = async (id) => {
   // seu código aqui
+  if (!id) throw new Error('You must provide an url');
+  const endPoint = `https://api.mercadolibre.com/items/${id}`;
+  const response = await fetch(endPoint);
+  const data = await response.json();
+  return data;
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -11,7 +11,7 @@ describe('2 - Teste a função fetchItem', () => {
     expect(typeof fetchItem).toBe('function');
   });
   test('verifica se fetch é chamado após executar fetchItem("MLB1615760527")', async () => {
-    expect.assertions(1);
+    expect.assertions(2);
     const url = 'https://api.mercadolibre.com/items/MLB1615760527';
     await fetchItem('MLB1615760527');
     expect(fetch).toHaveBeenCalled();
